5988a9ed9855c02488a26e97a3f50bb8b886c9f4
felix <qfl2000@gmail.com>
2016-04-16 19:38:18 +0800

    add x264 build shell
    Change-Id: I9b90d8735b8598774535af7ffaa636f03594b16e

---
+++ x264-20160415-stable/build.sh
@@ -0,0 +1,49 @@
+
+BUILD_PLATFORM=linux64
+
+THISDIR=`dirname $0`
+
+source ${THISDIR}/module.env
+
+
+echo "build start ..."
+
+echo "-------------------------------"
+if [ ! -f ${THISDIR}/${MODULE_PACKAGE} ]; then
+	echo "downloading ..."
+	curl ${MODULE_URL} > ${THISDIR}/${MODULE_PACKAGE} || exit 3
+fi
+
+echo "-------------------------------"
+if [ ! -d ${THISDIR}/${MODULE_SRCPATH} ]; then
+	echo "unpack ..."
+	tar jxvf ${MODULE_PACKAGE}
+fi
+
+cd ${THISDIR}/${MODULE_SRCPATH}
+
+echo "-------------------------------"
+echo "configure ..."
+./configure --prefix=`pwd`/../tmp-bin/	\
+	--disable-cli	\
+	--enable-shared
+
+echo "-------------------------------"
+echo "making ..."
+make -j4 || exit 1
+make install || exit 2
+
+
+echo "-------------------------------"
+cd ..
+echo "install platform files ..."
+
+mkdir -p include
+mkdir -p lib/${BUILD_PLATFORM}
+cp -dprf tmp-bin/include/* include/
+#cp -f tmp-bin/bin/*.dll lib/${BUILD_PLATFORM}/
+#cp -f tmp-bin/bin/*.lib lib/${BUILD_PLATFORM}/
+#cp -f tmp-bin/lib/*.a lib/${BUILD_PLATFORM}/
+cp -f tmp-bin/lib/*.so lib/${BUILD_PLATFORM}/
+
+
