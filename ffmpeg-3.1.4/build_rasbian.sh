a95b0f70a61b37e5e9ab595cf78b1a0ae3542dce
charles <qfl2000@gmail.com>
2018-10-06 09:58:32 +0800

    add build ffmpeg script for rasbian

---
+++ ffmpeg-3.1.4/build_rasbian.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+
+PLATFORM=rasbian
+
+
+THISDIR=`dirname $0`
+
+
+cd ${THISDIR}/ffmpeg-3.1.4
+
+
+echo "-------------------------------"
+echo "configure ..."
+./configure --prefix=`pwd`/../tmp-bin/	\
+	--enable-static	\
+	--disable-shared		\
+	--enable-memalign-hack	\
+	--disable-programs	\
+	--disable-doc	    \
+	--disable-avdevice	\
+	--disable-swresample	\
+	--disable-swscale 	\
+	--disable-postproc	\
+	--disable-avfilter	\
+	--disable-network	\
+	--disable-everything	\
+	--enable-protocol=file	\
+	--enable-omx \
+	--enable-omx-rpi \
+	--enable-encoder=h264_omx \
+	--enable-mmal \
+	--enable-hwaccel=h264_mmal \
+	--enable-decoder=h264_mmal \
+	|| exit 1
+
+
+echo "-------------------------------"
+echo "making ..."
+make -j4 || exit 2
+make install || exit 3
+
+
+echo "-------------------------------"
+cd ..
+echo "install platform files ..."
+
+mkdir -p include
+cp -av tmp-bin/include/* include/
+
+mkdir -p lib/${PLATFORM}
+cp -fv tmp-bin/lib/libavformat.a lib/${PLATFORM}/
+cp -fv tmp-bin/lib/libavcodec.a lib/${PLATFORM}/
+cp -fv tmp-bin/lib/libavutil.a lib/${PLATFORM}/
+
+
+rm -fr tmp-bin
+
